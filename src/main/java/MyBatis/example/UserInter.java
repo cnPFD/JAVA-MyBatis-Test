17f899b1eb8bf98268c0530d1eecac45ea51596b
wcc1106149162 <1106149162@qq.com>
2017-05-10 09:18:38 +0800

    同步仓库

---
+++ src/main/java/MyBatis/example/UserInter.java
@@ -0,0 +1,11 @@
+package MyBatis.example;
+
+/**
+ * Created by soft01 on 2017/5/9.
+ */
+public interface UserInter {
+    public User showusermsg(String idcardnum);
+    public void insertuser(User user);
+    public void deleteuser(String idcardnum);
+    public void updateuser(User user);
+}
